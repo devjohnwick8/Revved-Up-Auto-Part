b71126c4db06831c6b161eaafeaf22827decf2dc
devjohnwick8 <devjohnwick8@gmail.com>
2022-02-03 14:20:23 -0800

    Email working

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -11,8 +11,11 @@ class EmailController extends Controller
     //
     public function order_place($order_number){
         $order = OrderModel::with('get_shipping', 'get_user')->where('order_number', $order_number)->first();
+        dd($order->get_user['email']);
         if($order){
-            \Mail::to($order->get_user->email)->send(new MyTestMail($order));
+
+
+            \Mail::to($order->get_user['email'])->send(new MyTestMail($order));
         }else{
             return back()->with('Order Not Found');
         }
