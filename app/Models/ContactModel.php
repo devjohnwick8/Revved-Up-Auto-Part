5cd6f47d931a208a6499c8955d81f14600b73763
Devjohnwick <devjohnwick8@gmail.com>
2022-02-09 04:59:11 +0500

    fillable issue

--- app/Models/ContactModel.php
+++ app/Models/ContactModel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactModel extends Model
 {
-    protected $table= "contacts";
     use HasFactory;
+    protected $table= "contacts";
+    protected $guarded = [];
 }
